31cb61ddd25507a36330bae6d96cff688fc1ca4e
Albert Lanza Rio <albert2000.lanza@gmail.com>
2023-05-12 19:39:24 +0200

    Add Create Teams Table  migration

---
+++ database/migrations/2023_05_12_173746_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 45);
+            $table->string('country', 45);
+            $table->string('city', 45);
+            $table->string('stadium', 45);
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
